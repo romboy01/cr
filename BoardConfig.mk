cupid-development/android_device_xiaomi_marble

marble: Define device specific kernel modules
Change-Id: I33ee186c135e288904e0fc6c8401f578b3086059
Signed-off-by: Jens Reidel <adrian@travitia.xyz>

BoardConfig.mk
@@ -12,6 +12,17 @@ include vendor/xiaomi/marble/BoardConfigVendor.mk
 
 DEVICE_PATH := device/xiaomi/marble
 
+# Kernel modules
+BOOT_KERNEL_MODULES += \
+    qcom_pm8008-regulator.ko \
+    gt9916r.ko \
+    qcom-hv-haptics.ko \
+    fpc1540.ko \
+    goodix_3626.ko \
+    xiaomifp.ko
+BOARD_VENDOR_RAMDISK_RECOVERY_KERNEL_MODULES_LOAD += $(BOOT_KERNEL_MODULES)
+BOARD_VENDOR_KERNEL_MODULES_LOAD += $(BOOT_KERNEL_MODULES)
+
 # Properties
 TARGET_SYSTEM_PROP += $(DEVICE_PATH)/properties/system.prop
 TARGET_VENDOR_PROP += $(DEVICE_PATH)/properties/vendor.prop

romboy01/cr

BoardConfig.mk
@@ -12,6 +12,16 @@ include vendor/xiaomi/marble/BoardConfigVendor.mk
 
 DEVICE_PATH := device/xiaomi/marble
 
+# Kernel modules
+BOOT_KERNEL_MODULES += \
+    qcom_pm8008-regulator.ko \
+    gt9916r.ko \
+    qcom-hv-haptics.ko \
+    fpc1540.ko \
+    goodix_3626.ko
+BOARD_VENDOR_RAMDISK_RECOVERY_KERNEL_MODULES_LOAD += $(BOOT_KERNEL_MODULES)
+BOARD_VENDOR_KERNEL_MODULES_LOAD += $(BOOT_KERNEL_MODULES)
+
 # Properties
 TARGET_SYSTEM_PROP += $(DEVICE_PATH)/properties/system.prop
 TARGET_VENDOR_PROP += $(DEVICE_PATH)/properties/vendor.prop